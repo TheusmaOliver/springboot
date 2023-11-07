c24623799851a22ebfe55358b9192d5e99c6d84f
TheusmaOliver <matheusrodriguesoliveira273@gmail.com>
2023-11-07 17:42:51 -0300

    feat: create product table

---
+++ src/main/java/com/example/springboot/entities/Product.java
@@ -0,0 +1,27 @@
+package com.example.springboot.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_products")
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class Product implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private BigDecimal price;
+}
