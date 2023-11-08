ff5bbddcab46e0a4ab0f4a863de7ed3dfbb63561
TheusmaOliver <matheusrodriguesoliveira273@gmail.com>
2023-11-07 21:18:03 -0300

    fix: returning id in product dto

--- src/main/java/com/example/springboot/dto/ProductRecordDto.java
+++ src/main/java/com/example/springboot/dto/ProductRecordDto.java
@@ -5,9 +5,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
-public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal price) {
+public record ProductRecordDto(UUID id,@NotBlank String name, @NotNull BigDecimal price) {
     public ProductRecordDto(Product product){
-        this(product.getName(), product.getPrice());
+        this(product.getId(), product.getName(), product.getPrice());
     }
 }
+
